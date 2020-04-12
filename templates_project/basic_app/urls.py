ec617b92f0b71d4f5e1c7dabe73733ef49f71f43
captainmd <captainmd@gmail.com>
2020-04-12 22:23:44 +0530

    first

---
+++ templates_project/basic_app/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+app_name = 'basic_app'
+
+urlpatterns = [
+    path('relative_url/', views.relative, name = 'relative'),
+    path('other/', views.other, name = 'other')
+]
